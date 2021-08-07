1294564225f2b0330f305f2aeb148d95667f9281
Rafael Gustavo Grassani <grassanir@gmail.com>
2021-08-06 21:58:00 -0300

    Adicionei as Activities Histórico Fotos e Regas, mudei o menu do details, e deixei apenas um botão regar na tela details

---
+++ app/src/main/java/com/android/e_garden/Photo_History.java
@@ -0,0 +1,15 @@
+
+package com.android.e_garden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Photo_History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_history);
+    }
+}
---
+++ app/src/main/java/com/android/e_garden/Water_History.java
@@ -0,0 +1,14 @@
+package com.android.e_garden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Water_History extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_history);
+    }
+}
