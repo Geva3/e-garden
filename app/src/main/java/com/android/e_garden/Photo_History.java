9c036ed64e9eff19b6312e4d5712c704bd321e02
Rafael Gustavo Grassani <grassanir@gmail.com>
2021-08-06 22:30:22 -0300

    Adicionei as Activities Histórico Fotos e Regas, mudei o menu do details, e deixei apenas um botão regar na tela details, adicionei título nas activities

--- app/src/main/java/com/android/e_garden/Photo_History.java
+++ app/src/main/java/com/android/e_garden/Photo_History.java
@@ -2,6 +2,7 @@
 package com.android.e_garden;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -11,5 +12,14 @@ public class Photo_History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_history);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_photo_history);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        setTitle("Histórico de Fotos");
+
+
     }
 }
--- app/src/main/java/com/android/e_garden/Water_History.java
+++ app/src/main/java/com/android/e_garden/Water_History.java
@@ -1,6 +1,7 @@
 package com.android.e_garden;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,14 @@ public class Water_History extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_water_history);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_water_history);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        setTitle("Histórico de Regas");
+
+
     }
 }
